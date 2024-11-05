a0149ca6533667480da28d0127b7c7a267c2622c
Erik Svanes <svanes.erik@gmail.com>
2024-11-05 14:16:53 -0500

    gradient

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
     // const [count, setCount] = useState(0)
 
     return (
-        <div className='app flex items-center justify-center bg-gray-800'>
-            <h1 className='text-green-500 text-xl'>
-                Hi Catherine! {'<'}3
+        <div className='app flex items-center justify-center bg-gradient-to-br from-green-300 to-teal-950'>
+            <h1 className='text-blue-900 text-2xl font-bold'>
+                {'<'}3 Hi Catherine! {'<'}3
             </h1>
         </div>
     )
