793248c59190d43aa69b732cc4cc93f0d553f098
Erik Svanes <svanes.erik@gmail.com>
2024-11-05 13:53:54 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <div className='app flex items-center justify-center bg-gray-800'>
+            <h1 className='text-green-500 text-xl'>
+                Hi Catherine! {'<'}3
+            </h1>
+        </div>
+    )
+}
+
+export default App
